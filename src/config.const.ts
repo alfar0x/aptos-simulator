38c6a963cb011966124c281e5f0f8b3f7a6fd364
John Doe <johndoe@example.com>
2023-12-26 13:25:35 +0200

    minor

--- src/config.const.ts
+++ src/config.const.ts
@@ -1,11 +1,11 @@
 export const sleepBeforeStartAccountSecMin = 10;
-export const sleepBeforeStartAccountSecMax = 60 * 60 * 60;
+export const sleepBeforeStartAccountSecMax = 60 * 60 * 36;
 
 export const txAmountMin = 40;
 export const txAmountMax = 80;
 
 export const sleepBetweenTransactionsSecMin = 60 * 10;
-export const sleepBetweenTransactionsSecMax = 60 * 60 * 30;
+export const sleepBetweenTransactionsSecMax = 60 * 60 * 36;
 
 export const sortField = 'next_step_time';
 export const sortOrder = 'desc'
